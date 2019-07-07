8c5d8f15e3fee59a6cb13aaeefe6a65adbb25318
vishald1994 <49125707+vishald1994@users.noreply.github.com>
2019-07-06 21:12:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,216 +1,147 @@
-///Start button on the Start Page takes user to the first question
-//When an answer is selected, turn white circle to blue
-//when user submits answer choice it will display correct/incorrect result on next page
-//final score will be displayed on final score page
-//Try again button displayed on final score page 
-//need a function to display start page to document
-$(function () {
-  beginQuiz();
-  handleAnswerFeedback();
-  handleAnswerSubmits();
-  renderQuestionPage();
+let questionNumber = 0;
+let score = 0;
+
+//generate question html
+function generateQuestion () {
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form>
+    <fieldset>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[0]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[1]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[2]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[3]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+} else {
+    renderResults();
+    restartQuiz();
+    $('.questionNumber').text(10)
+  }
+}
+
+//increment question number
+function changeQuestionNumber () {
+  //if (questionNumber < STORE.length) {
+    questionNumber ++;
+  //}
+  $('.questionNumber').text(questionNumber+1);
+}
+
+//increment score
+function changeScore () {
+  score ++;
+}
+
+//start quiz
+//on startQuizButton click hide start div
+//unhide quiz form div
+function startQuiz () {
+  $('.quizStart').on('click', '.startButton', function (event) {
+    $('.quizStart').remove();
+    $('.questionAnswerForm').css('display', 'block');
+    $('.questionNumber').text(1);
 });
+}
 
-var state = {
-  questions: [
-    {//question 1
-      question: 'What is generally considered to be the first "pony car"?',
-      answers: ['Pontiac Firebird',
-                'Mercury Cougar',
-                'Ford Mustang',
-                'Chevrolet Camaro'],
-      correctAnswer: 'Ford Mustang'
-    },
-    {//question 2
-      question:'What was the first Japanese car to be produced in the US?',
-      answers: ['Nissan Maxima', 'Honda Accord', 'Mazda Miata', 'Toyota Camry'],
-      correctAnswer: 'Honda Accord'
-    },
-    {//question 3
-      question:'Which animal is on the Porsche logo?',
-      answers: ['Lion', 'Bull', 'Bird', 'Horse'],
-      correctAnswer: 'Horse'
-    },
-    {//question 4
-      question:'What car sold more than one million units in 1965, setting a record that still stands today?',
-      answers: ['Buick Wildcat', 'Chevrolet Impala', 'Pontiac GTO', 'Ford Thunderbird'],
-      correctAnswer: 'Chevrolet Impala'
-    },
-    {//question 5
-      question:'What was the first car to break the sound barrier?',
-      answers: ['Koenigsegg Agera R', 'Hennessey Venom GT', 'Bugatti Veyron', 'Thrust SSC'],
-      correctAnswer: 'Thrust SSC'
-    },
-    {//question 6
-      question:'What year was the corvette first introduced?',
-      answers: ['1943', '1973', '1963', '1953'],
-      correctAnswer: '1953'
-    },
-    {//question 7
-      question: 'What was the first car to be mass-produced?',
-      answers: ['Model T', 'Packard', 'Duryea Motor Wagon', 'Model A'],
-      correctAnswer: 'Model T'
-    },
-    {//question 8
-      question:'What was the first car launched into space?',
-      answers: ['Rolls-Royce Vsion 100', 'Porsche 911', 'Tesla Roadster', 'Rinspeed Oasis'],
-      correctAnswer: 'Tesla Roadster'
-    },
-    {//question 9
-      question:'The inventor of cruise control was?',
-      answers: ['A 12 year-old child', 'A Homeless Man', 'A Blind Man', 'A Robot'],
-      correctAnswer: 'A Blind Man'
-    },
-    {//question 10
-      question:'How much did the first Ford Mustang cost?',
-      answers: ['$3,484', '$2,368', '$1,652', '$2,944'],
-      correctAnswer: '$2,368'
+// render question in DOM
+function renderQuestion () {
+  $('.questionAnswerForm').html(generateQuestion());
+}
+
+//user selects answer on submit run user feedback
+function userSelectAnswer () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    if (answer === correctAnswer) {
+      selected.parent().addClass('correct');
+      ifAnswerIsCorrect();
+    } else {
+      selected.parent().addClass('wrong');
+      ifAnswerIsWrong();
     }
-    ],
-    currentQuestionIndex: 0,
-    correctCount: 0,
-    quizStarted: false,
-      // need to have the feedback box cleared
-    // renderQuestionFeedback: text()
-};
-
-function resetQuiz() {
-  state.correctCount = 0;
-  state.currentQuestionIndex = 0;
+  });
 }
 
-//need a function to display:question number, corresponding question and answer choices, and a submit (Next) button
-//need a function to close popup and move to/display next question
-function renderQuestionPage() {
-  var currentQuestionObj = state.questions[state.currentQuestionIndex];
+function ifAnswerIsCorrect () {
+  userAnswerFeedbackCorrect();
+  updateScore();
+}
+
+function ifAnswerIsWrong () {
+  userAnswerFeedbackWrong();
+}
+
+//user feedback for correct answer
+function userAnswerFeedbackCorrect () {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"></div><p><b>You got it right!</b></p><button type=button class="nextButton">Next</button></div>`);
+}
+
+//user feedback for wrong answer
+function userAnswerFeedbackWrong () {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"></div><p><b>You got it wrong</b><br>the correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+}
+
+//update score text
+function updateScore () {
+  changeScore();
+  $('.score').text(score);
+}
+
+//when quiz is over this is the html for the page
+function renderResults () {
+  if (score >= 8) {
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You are a Car History Expert!</h3><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYFos8cF5evSQLkpUaaOYv6E2aRPE13a_1rdBz2ML7B8efjYka" alt="car expert icon"/><p>You got ${score} / 10</p><p>Great Job!</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else if (score < 8 && score >= 5) {
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>Almost there!</h3><img src="https://d3vl3jxeh4ou3u.cloudfront.net/1%20-%20How%20to%20Move%20a%20Stranded%20Car%20-%20%20Person%20sitting%20near%20their%20broken%20down%20car%20on%20the%20side%20of%20the%20road%2C%20with%20the%20hood%20open.jpg" alt="broke down car with guy"/><p>You got ${score} / 10</p><p>Read up on your Car History knowledge a little more and you'll be ready to go!</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else {
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You might wanna read up some more on Car History</h3><img src="https://d3vl3jxeh4ou3u.cloudfront.net/1%20-%20How%20to%20Move%20a%20Stranded%20Car%20-%20%20Person%20sitting%20near%20their%20broken%20down%20car%20on%20the%20side%20of%20the%20road%2C%20with%20the%20hood%20open.jpg" alt="broke down car with guy"/><p>You got ${score} / 10</p><p>With more reading about car history you should pass next time!</p><button class="restartButton">Restart Quiz</button></div>`);
+  }
+}
+
+//what happens when the user clicks next
+function renderNextQuestion () {
+  $('main').on('click', '.nextButton', function (event) {
+    changeQuestionNumber();
+    renderQuestion();
+    userSelectAnswer();
+  });
+}
+
+//restart quiz function - reloads page to start quiz over
+function restartQuiz () {
+  $('main').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+}
+
+//run quiz functions
+function createQuiz () {
+  startQuiz();
   renderQuestion();
-  renderQuestionChoices(currentQuestionObj.answers);
-  // $(".popup-inner").addClass("hidden");
+  userSelectAnswer();
+  renderNextQuestion();
 }
 
-function renderQuestion() {
-  var progressHTML = "<span>(" + (state.currentQuestionIndex + 1) + '/' + state.questions.length + ")</span>"
-  var questionText = state.questions[state.currentQuestionIndex].question;
-  $(".js-question-text").html(progressHTML + questionText);
-  // console.log(renderQuestion);
-}
+$(createQuiz);
 
-function renderQuestionChoices(answers) {
-  $("#question-form label").each(function (index,label) {
-    $(this).find("input").attr("value", answers[index]);
-    $(this).find("input").prop("checked", false);
-    $(this).find("span").text(answers[index]);
-    // $("#submit-answer").addClass("hidden");
-    //is this where I might add in code to make an answer choice REQUIRED?
-    //also add a removeClass("hidden") on the #submit-answer button if I decide to go that route?
-  });
-}
-
-//function to display results
-function renderFinalResults() {
-  $("#my-quiz").addClass("hidden");
-  $("#results-page").removeClass("hidden");
-  $("#retake-button").removeClass("hidden");
-  var element = $(".js-final-results");
-  element.html("<h2>" + "You got" + ' ' + state.correctCount + ' ' + "out of" + ' ' + state.questions.length + ' ' + "correct" + "</h2>");
-  handleQuizRestart();
-}
-
-function checkAnswer(userChoice) {
-  var correctChoice = state.questions[state.currentQuestionIndex].correctAnswer;
-  // console.log(state.currentQuestionIndex,state.questions[state.currentQuestionIndex]);
-  if (userChoice === correctChoice) {
-    state.correctCount++;
-    renderQuestionFeedback(true);
-    // $("#submit-answer").removeClass("hidden");
-    state.currentQuestionIndex++;
-  }
-  else if (userChoice === undefined) {
-    renderQuestionFeedback ("unanswered");
-  }
-  else {
-    renderQuestionFeedback (false);
-    state.currentQuestionIndex++;
-  }
-  if (state.currentQuestionIndex == state.questions.length) {
-    renderFinalResults()
-  }
-  else {
-    renderQuestionPage();
-  }
-  // console.log(userChoice);
-}
-
-//need a function to call "popup-feedback" and display in DOM
-function renderQuestionFeedback(response) {
-  var feedback = $(".popup-inner");
-  if (response === true) {
-    feedback.find("h2").text("That was correct!");
-  }
-  else if (response === false) {
-    feedback.find("h2").text("Sorry your answer was not correct.");
-  }
-  else if (response === "unanswered") {
-    feedback.find("h2").text("Must answer question");
-  }
-}
-
-//need a function to change the page to question page upon button click (Take Quiz!)
-function beginQuiz() {
- $("#start-button").click(function (e) {
-   $("#my-quiz").removeClass("hidden");
-   $("#start-button").addClass("hidden");
-  // console.log("take quiz clicked");
- }); 
-}
-
-function handleQuizRestart() {
-    $("#retake-button").click(function (e) {
-  // $("#start-quiz").click(function (e) {
-    $("#my-quiz").removeClass("hidden");
-    $("#retake-button").addClass("hidden");
-    $("#txt-feedback").empty();
-    $(".js-final-results").text("");
-    resetQuiz();
-    renderQuestionPage();
-  });
-}
-
-
-//need a function to evaluate whether user is correct/incorrect
-//should this be coded as a loop to evaluate if answer is correct/incorrect?
-function handleAnswerSubmits() {
-  $("#submit-answer").click(function (e) {
-  e.preventDefault();
-  // $(".popup-inner").removeClass("hidden");
-  var userChoice = $("input[name='answerChoice']:checked").val();
-  renderQuestionFeedback();
-  checkAnswer(userChoice);
-  });
-}
-
-//need a function to open a feedback window after answer is submitted
-function handleAnswerFeedback() {
-  // to open...
-  $("#submit-answer").on("click", function (e) {
-    e.preventDefault();
-    var targetPopupClass = $(this).attr("data-popup-open");
-    $(`[data-popup="' + targetPopupClass + '"]`).fadeIn(250);
-    e.preventDefault();
-  });
-  //to close...
-  $("#close-feedback-modal").on("click", function (e) {
-    var targetPopupClass = $(this).attr("data-popup-close");
-    $(`[data-popup = "' + targetPopupClass + '"]`).fadeOut(250);
-    e.preventDefault();
-    renderQuestionFeedback();
-  });
-}
-
-
-
-//need  function(s) to display results (# out of 10), final message, display "retake-button" which should go back to start page
-function resetQuiz() {
-  state.correctCount = 0;
-  state.currentQuestionIndex = 0;
-}
