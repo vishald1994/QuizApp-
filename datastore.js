fc197d7e8579fc8be1c34e367cc48a2a5311fe25
vishald1994 <49125707+vishald1994@users.noreply.github.com>
2019-07-06 21:08:06 -0400

    Create datastore.js

---
+++ datastore.js
@@ -0,0 +1,55 @@
+const STORE = [
+    {//question 1
+      question: 'What is generally considered to be the first "pony car"?',
+      answers: ['Pontiac Firebird',
+                'Mercury Cougar',
+                'Ford Mustang',
+                'Chevrolet Camaro'],
+      correctAnswer: 'Ford Mustang'
+    },
+    {//question 2
+      question:'What was the first Japanese car to be produced in the US?',
+      answers: ['Nissan Maxima', 'Honda Accord', 'Mazda Miata', 'Toyota Camry'],
+      correctAnswer: 'Honda Accord'
+    },
+    {//question 3
+      question:'Which animal is on the Porsche logo?',
+      answers: ['Lion', 'Bull', 'Bird', 'Horse'],
+      correctAnswer: 'Horse'
+    },
+    {//question 4
+      question:'What car sold more than one million units in 1965, setting a record that still stands today?',
+      answers: ['Buick Wildcat', 'Chevrolet Impala', 'Pontiac GTO', 'Ford Thunderbird'],
+      correctAnswer: 'Chevrolet Impala'
+    },
+    {//question 5
+      question:'What was the first car to break the sound barrier?',
+      answers: ['Koenigsegg Agera R', 'Hennessey Venom GT', 'Bugatti Veyron', 'Thrust SSC'],
+      correctAnswer: 'Thrust SSC'  
+    },
+    {//question 6
+      question:'What year was the corvette first introduced?',
+      answers: ['1943', '1973', '1963', '1953'],
+      correctAnswer: '1953'
+    },
+    {//question 7
+      question: 'What was the first car to be mass-produced?',
+      answers: ['Model T', 'Packard', 'Duryea Motor Wagon', 'Model A'],
+      correctAnswer: 'Model T'
+    },
+    {//question 8
+      question:'What was the first car launched into space?',
+      answers: ['Rolls-Royce Vsion 100', 'Porsche 911', 'Tesla Roadster', 'Rinspeed Oasis'],
+      correctAnswer: 'Tesla Roadster'
+    },
+    {//question 9
+      question:'The inventor of cruise control was?',
+      answers: ['A 12 year-old child', 'A Homeless Man', 'A Blind Man', 'A Robot'],
+      correctAnswer: 'A Blind Man' 
+    },
+    {//question 10
+      question:'How much did the first Ford Mustang cost?',
+      answers: ['$3,484', '$2,368', '$1,652', '$2,944'],
+      correctAnswer: '$2,368'
+    }
+];
